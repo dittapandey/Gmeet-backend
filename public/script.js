58735ecec0381c245c9a8ecc861cfab78704dbf0
dittapandey <adityapandey51020@gmail.com>
2021-12-29 12:31:50 +0530

    Peer js port modified

--- public/script.js
+++ public/script.js
@@ -26,7 +26,7 @@ const user = prompt("Enter your name");
 var peer = new Peer(undefined, {
     path: "peerjs",
     host: "/",
-    port: "443"
+    port: process.env.PORT || "3030"
 });
 
 let myVideoStream;
